f6caf9b0b18eab8181e5bb12f90f3436eab50c06
Ana Pineda <anamichellepd@gmail.com>
2020-05-21 19:47:49 -0700

    working on the part where there is no user or it is not loading

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,32 @@ import "bulma/css/bulma.css";
 import { Auth0Context } from "./contexts/auth0-context";
 
 function App() {
+  const { isLoading, user, loginWithRedirect } = useContext(Auth0Context);
   const auth0 = useContext(Auth0Context);
   return (
     <div className="hero is-info is-fullheight">
       <div className="hero-body">
         <div className="container has-text-centered">
-          <h1>Click Below!</h1>
-          <button
-            onClick={auth0.loginWithRedirect}
-            className="button is-danger"
-          >
-            Login
-          </button>
+          {!isLoading && !user && (
+            <>
+              <h1>Click Below!</h1>
+              <button
+                onClick={auth0.loginWithRedirect}
+                className="button is-danger"
+              >
+                Login
+              </button>
+            </>
+          )}
+          {/* this is the new section */}
+          {!isLoading && user && (
+            <>
+              <h1>You are logged in!</h1>
+              <p>Hello {user.name}</p>
+
+              {user.picture && <img src={user.picture} alt="My Avatar" />}
+            </>
+          )}
         </div>
       </div>
     </div>
