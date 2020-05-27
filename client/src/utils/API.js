77cc4a8f1950021798f3f315f5d27519fbbae738
Ana Pineda <anamichellepd@gmail.com>
2020-05-27 12:48:54 -0700

    working on first part of Mongo

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+  // Gets all logs
+  getLogs: function () {
+    return axios.get("/api/logs");
+  },
+  // Gets the log with the given id
+  getLog: function (id) {
+    return axios.get("/api/logs/" + id);
+  },
+  // Deletes the log with the given id
+  deleteLog: function (id) {
+    return axios.delete("/api/logs/" + id);
+  },
+  // Saves a log to the database
+  saveLog: function (logData) {
+    return axios.post("/api/logs", logData);
+  },
+};
