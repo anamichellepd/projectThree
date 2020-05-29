d63febb012b316266f7cc39173f3315a834db427
Ana Pineda <anamichellepd@gmail.com>
2020-05-28 21:32:26 -0700

    working on deploying to heroku

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -9,54 +9,56 @@ export default function Header() {
   return (
     <>
       <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between headerNav">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between headerNav">
           <Link to="/" id="logoHeader" className="navbar-item">
             PSYKHE
           </Link>
 
           {!isLoading && user && (
-            <form class="form-inline">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                  <Link to="/" className="nav-link">
-                    Home
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link to="/New" className="nav-link">
-                    New Log
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link to="/Past" className="nav-link">
-                    Past Logs
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link to="/Insights" className="nav-link">
-                    Insights
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <Link to="/Results" className="nav-link">
-                    Results
-                  </Link>
-                </li>
-                <li class="nav-item">
-                  <button
-                  type="button"
-
-                    onClick={() => logout({ returnTo: window.location.origin })}
-                    className="navbar-item btn btn-light headerLogoutBtn"
-                  >
-                    {user.name}: Logout
-                  </button>
-                </li>
-              </ul>
-              
-            </div>
+            <form className="form-inline">
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item">
+                    <Link to="/" className="nav-link">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/New" className="nav-link">
+                      New Log
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/Past" className="nav-link">
+                      Past Logs
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/Insights" className="nav-link">
+                      Insights
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/Results" className="nav-link">
+                      Results
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <button
+                      type="button"
+                      onClick={() =>
+                        logout({ returnTo: window.location.origin })
+                      }
+                      className="navbar-item btn btn-light headerLogoutBtn"
+                    >
+                      {user.name}: Logout
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </form>
           )}
         </nav>
