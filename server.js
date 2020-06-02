3c6036f3d87c9dec95ed592ca6712d4c355fbb6c
Ana Pineda <anamichellepd@gmail.com>
2020-06-02 16:04:13 -0700

    commented out the IBM stuff

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const jwt = require("express-jwt");
-const { IamTokenManager } = require('ibm-watson/auth');
+const { IamTokenManager } = require("ibm-watson/auth");
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
-if (!process.env.PERSONALITY_INSIGHTS_APIKEY) {
-  console.log('This example requires the PERSONALITY_INSIGHTS_APIKEY environment variable');
-  process.exit(1);
-}
+// if (!process.env.PERSONALITY_INSIGHTS_APIKEY) {
+//   console.log('This example requires the PERSONALITY_INSIGHTS_APIKEY environment variable');
+//   process.exit(1);
+// }
 
-const personalityAuthenticator = new IamTokenManager({
-  apikey: process.env.PERSONALITY_INSIGHTS_APIKEY,
-});
+// const personalityAuthenticator = new IamTokenManager({
+//   apikey: process.env.PERSONALITY_INSIGHTS_APIKEY,
+// });
 
 //if deployed, use the deployed databas. otherwise use the local psykhe database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/psykhe";
@@ -52,11 +52,14 @@ app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
 
-app.get('/api/token', function (req, res) {
+app.get("/api/token", function (req, res) {
   return personalityAuthenticator
     .requestToken()
     .then(({ result }) => {
-      res.json({ accessToken: result.access_token, url: process.env.PERSONALITY_INSIGHTS_URL });
+      res.json({
+        accessToken: result.access_token,
+        url: process.env.PERSONALITY_INSIGHTS_URL,
+      });
     })
     .catch(console.error);
 });
